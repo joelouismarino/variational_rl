7e18e3eb87ffa7469aea4ae5926c7c645c76075b
Alexandre Piche <apiche@Alexandres-MacBook-Pro-3.local>
2019-08-27 15:09:41 -0400

    added 3 unit tests

--- misc/retrace.py
+++ misc/retrace.py
@@ -13,10 +13,11 @@ def retrace(q_values, rewards, importance_weights=None, discount=0.9, l=0.9):
         # On-policy
         importance_weights = torch.ones_like(q_values)
 
-    deltas = q_values[:, :-1] - (rewards[:, 1:] + discount*q_values[:, 1:])
-    importance_weights = discount * l * torch.clamp(importance_weights, 0, 1)[:, :-2]
+    deltas = rewards[:, :-1] + discount*q_values[:, 1:] - q_values[:, :-1]
+    importance_weights = l * torch.clamp(importance_weights, 0, 1)[:, :-2]
     importance_weights = torch.cat([torch.ones_like(importance_weights[:, :1]), importance_weights], 1)
-    q_estimates = q_values[:, :1] + torch.sum(torch.cumprod(importance_weights, 1) * deltas, 1, keepdim=True)
+    discounts = torch.cat([(discount*torch.ones_like(q_values[:, :1]))**i for i in range(q_values.shape[1])], 1)
+    q_estimates = q_values[:, :1] + torch.sum(discounts[:, :-1] * torch.cumprod(importance_weights, 1) * deltas, 1, keepdim=True)
 
     return q_estimates
 
@@ -27,9 +28,27 @@ if __name__ == '__main__':
     importance_weights = torch.rand((10, 9))
     q_values = torch.rand((10, 9))
     rewards = torch.rand((10, 9))
-    one_target = retrace(q_values, rewards, importance_weights)
-    print(one_target)
-    multiple_targets = torch.cat([retrace(q_values[:, i:], rewards[:, i:], importance_weights[:, i:]) for i in range(q_values.shape[1])], 1)
-    print(multiple_targets)
+    # How to perform multiple targets
+    # multiple_targets = torch.cat([retrace(q_values[:, i:], rewards[:, i:], importance_weights[:, i:]) for i in range(q_values.shape[1])], 1)
+
+    print('Test degenerate case lambda is 0 and discount is 0')
+    print('The target should be equal to the first reward')
+    one_target = retrace(q_values, rewards, importance_weights, l=0., discount=0.)
+    assert torch.all(one_target == rewards[:, :1])
+
+    print('Test Monte Carlo rollouts')
+    print('The target should be the undiscounted sum of reward plus the value function')
+    one_target = retrace(q_values, rewards, None, discount=1., l=1.)
+    mc_rollouts = torch.sum(rewards[:, :-1], 1, keepdim=True) + q_values[:, -1:]
+    assert torch.all(torch.isclose(one_target, mc_rollouts))
+
+    print('Test Discounted Monte Carlo rollouts')
+    print('The target should be the iscounted sum of reward plus the value function')
+    one_target = retrace(q_values, rewards, None, discount=.99, l=1.)
+    discounts = torch.cat([(0.99*torch.ones_like(q_values[:, :1]))**i for i in range(q_values.shape[1])], 1)
+    disc_mc_rollouts = torch.sum((discounts*rewards)[:, :-1], 1, keepdim=True) + 0.99**(q_values.shape[1]-1)*q_values[:, -1:]
+    assert torch.all(torch.isclose(one_target, disc_mc_rollouts))
+
+
 
 
