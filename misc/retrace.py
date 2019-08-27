266ab4e179e4f30bb3d4d22deb99128d21512f86
Alexandre Piche <apiche@Alexandres-MacBook-Pro-3.local>
2019-08-27 14:35:36 -0400

    retrace

---
+++ misc/retrace.py
@@ -0,0 +1,30 @@
+import torch
+
+
+def retrace(q_values, rewards, importance_weights=None, discount=0.9, l=0.9):
+    if q_values.shape[0] == 1:
+        return q_values
+
+    if importance_weights is None:
+        importance_weights = torch.ones_like(q_values)
+
+    deltas = q_values[:, :-1] - (rewards[:, 1:] + discount*q_values[:, 1:])
+    importance_weights = discount * l * torch.clamp(importance_weights, 0, 1)[:, :-2]
+    importance_weights = torch.cat([torch.ones_like(importance_weights[:, :1]), importance_weights], 1)
+    q_estimates = q_values[:, :1] + discount*torch.sum(torch.cumprod(importance_weights, 1) * deltas, 1, keepdim=True)
+
+    return q_estimates
+
+
+if __name__ == '__main__':
+    import torch
+
+    importance_weights = torch.rand((10, 9))
+    q_values = torch.rand((10, 9))
+    rewards = torch.rand((10, 9))
+    one_target = retrace(q_values, rewards, importance_weights)
+    print(one_target)
+    multiple_targets = torch.cat([retrace(q_values[:, i:], rewards[:, i:], importance_weights[:, i:]) for i in range(q_values.shape[1])], 1)
+    print(multiple_targets)
+
+
