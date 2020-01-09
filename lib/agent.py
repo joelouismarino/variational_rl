Found bug in KL-div eval. CEM now runs.

lib/agent.py
@@ -93,10 +93,10 @@ class Agent(nn.Module):
 
     def estimate_objective(self, state, action):
         # estimates the objective (value)
-        kl = kl_divergence(self.approx_post, self.prior, sample=action).sum(dim=1, keepdim=True)
+        kl = kl_divergence(self.approx_post, self.prior, n_samples=self.n_action_samples, sample=action).sum(dim=1, keepdim=True)
         expanded_state = state.repeat(self.n_action_samples, 1)
         cond_log_like = self.q_value_estimator(self, expanded_state, action, detach_params=True)
-        return cond_log_like - self.alphas['pi'] * kl
+        return cond_log_like - self.alphas['pi'] * kl.repeat(self.n_action_samples, 1)
 
     def evaluate(self):
         # evaluate the objective, collect various metrics for reporting

lib/distributions/kl_divergence.py
@@ -21,16 +21,20 @@ def kl_divergence(dist1, dist2, n_samples=1, sample=None):
             w = w.view(n_s, -1, 1).repeat(1, 1, s.shape[-1])
         else:
             w = 1.
+
             s = sample if sample is not None else d1.sample(n_s)
         kl = d1.log_prob(s, non_planning=True) - d2.log_prob(s, non_planning=True)
         kl = w * kl
+        # NOTE: this n_s might not be the same as the number of samples, in which
+        #       case the view will be wrong...
         kl = kl.view(n_s, -1, kl.shape[-1])
         return kl.mean(dim=0)
 
     try:
         kl = torch.distributions.kl_divergence(dist1.dist, dist2.dist)
-        if sample is not None:
-            n_samples = int(sample.shape[0] / kl.shape[0])
-        return kl.repeat(n_samples, 1)
+        # if sample is not None:
+        #     n_samples = int(sample.shape[0] / kl.shape[0])
+        # return kl.repeat(n_samples, 1)
+        return kl
     except NotImplementedError:
         return numerical_approx(dist1, dist2, n_samples)