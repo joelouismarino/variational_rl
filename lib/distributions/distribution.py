Fixed issue with iterative and gradient.

lib/distributions/distribution.py
@@ -89,7 +89,7 @@ class Distribution(nn.Module):
             req_grad = False if constant else True
             constraint = self.dist_type.arg_constraints[param]
             if type(constraint) == constraints.greater_than and constraint.lower_bound == 0:
-                self.initial_params[param] = nn.Parameter(torch.ones(1, n_variables))
+                self.initial_params[param] = nn.Parameter(0.5 * torch.ones(1, n_variables))
             elif constraint == constraints.dependent and param == 'low':
                 self.initial_params[param] = nn.Parameter(-torch.ones(1, n_variables), requires_grad=False)
             elif constraint == constraints.dependent and param == 'high':
@@ -321,8 +321,8 @@ class Distribution(nn.Module):
             params.extend(list(self.models[model_name].parameters()))
             if self.update != 'direct':
                 params.extend(list(self.gates[model_name].parameters()))
-        for param_name in self.initial_params:
-            params.append(self.initial_params[param_name])
+        # for param_name in self.initial_params:
+        #     params.append(self.initial_params[param_name])
         if self.const_scale:
             params.append(self.log_scale)
         return params

lib/inference/gradient.py
@@ -26,7 +26,7 @@ class GradientBasedInference(object):
         for _ in range(self.n_inf_iters):
             actions = agent.approx_post.sample(agent.n_action_samples)
             obj = agent.estimate_objective(state, actions)
-            obj = obj.view(agent.n_action_samples, -1, 1).mean(dim=0)
+            obj = - obj.view(agent.n_action_samples, -1, 1).mean(dim=0)
             self.estimated_objectives.append(obj.detach())
             obj.sum().backward(retain_graph=True)
             act_opt.step()

lib/inference/iterative.py
@@ -25,7 +25,7 @@ class IterativeInferenceModel(nn.Module):
             # sample actions, evaluate objective, backprop to get gradients
             actions = agent.approx_post.sample(agent.n_action_samples)
             obj = agent.estimate_objective(state, actions)
-            obj = obj.view(agent.n_action_samples, -1, 1).mean(dim=0)
+            obj = - obj.view(agent.n_action_samples, -1, 1).mean(dim=0)
             self.estimated_objectives.append(obj.detach())
             # TODO: should this be multiplied by valid and done?
             obj.sum().backward(retain_graph=True)