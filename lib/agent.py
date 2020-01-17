Fixed bug; was propagating gradients to model during inference. Model-based runs.

lib/agent.py
@@ -78,14 +78,20 @@ class Agent(nn.Module):
                 action = self.approx_post.sample().detach()
         self.collector.collect(state, action, reward, done, valid, log_prob)
         self.step(state, action)
-        action = action.tanh() if self.postprocess_action else action
+        action = action.tanh() if self.postprocess_action and self.mode == 'eval' else action
         return action.cpu().numpy()
 
-    def generate_prior(self, state):
+    def generate_prior(self, state, detach_params=False):
         # generates the action prior
         if self.prior_model is not None:
-            self.prior.step(self.prior_model(state=state))
-            self.target_prior.step(self.target_prior_model(state=state))
+            if detach_params:
+                prior_model = copy.deepcopy(self.prior_model)
+                target_prior_model = copy.deepcopy(self.target_prior_model)
+            else:
+                prior_model = self.prior_model
+                target_prior_model = self.target_prior_model
+            self.prior.step(prior_model(state=state), detach_params=detach_params)
+            self.target_prior.step(target_prior_model(state=state), detach_params=detach_params)
 
     def inference(self, state):
         # infers the action approximate posterior
@@ -100,6 +106,8 @@ class Agent(nn.Module):
         return cond_log_like - self.alphas['pi'] * kl.repeat(self.n_action_samples, 1)
 
     def step(self, state, action):
+        # updates the previous state and action (for state and reward prediction)
+        action = action.tanh() if self.postprocess_action and self.mode == 'eval' else action
         self._prev_action = action; self._prev_state = state
         self.q_value_estimator.set_prev_state(state)
 
@@ -166,7 +174,7 @@ class Agent(nn.Module):
         if self.prior_model is not None:
             self.prior_model.reset(batch_size)
             self.target_prior_model.reset(batch_size)
-        self.q_value_estimator.reset(batch_size, prev_action, prev_state)
+        self.q_value_estimator.reset(batch_size, prev_state)
         self.inference_optimizer.reset(batch_size)
 
         # reset the collector

lib/distributions/distribution.py
@@ -1,3 +1,4 @@
+import copy
 import torch
 import torch.nn as nn
 import torch.distributions.constraints as constraints
@@ -98,20 +99,36 @@ class Distribution(nn.Module):
                 self.initial_params[param] = nn.Parameter(torch.zeros(1, n_variables))
             self.initial_params[param].requires_grad_(req_grad)
 
-    def step(self, input=None, **kwargs):
+    def step(self, input=None, detach_params=False, **kwargs):
         """
         Update the distribution parameters by applying the models to the input.
 
         Args:
             input (torch.Tensor, optional): the input to the linear layers to
                                             the distribution parameters
+            detach_params (bool): whether to use detached (copied) parameters
         """
+        if detach_params:
+            models = {model_name: copy.deepcopy(model) for model_name, model in self.models.items()}
+            initial_params = {param_name: copy.deepcopy(param) for param_name, param in self.initial_params.items()}
+            if self.update != 'direct':
+                gates = {gate_name: copy.deepcopy(gate) for gate_name, gate in self.gates.items()}
+            if self.const_scale:
+                const_log_scale = copy.deepcopy(self.log_scale)
+        else:
+            models = self.models
+            initial_params = self.initial_params
+            if self.update != 'direct':
+                gates = self.gates
+            if self.const_scale:
+                const_log_scale = self.log_scale
+
         parameters = {}
         if input is not None:
-            for ind, param_name in enumerate(self.models):
+            for ind, param_name in enumerate(models):
                 constraint = self.dist.arg_constraints[param_name]
                 param_input = input[ind] if type(input) == list else input
-                param_update = self.models[param_name](param_input)
+                param_update = models[param_name](param_input)
                 if self.update == 'direct':
                     param = param_update
                 else:
@@ -119,7 +136,7 @@ class Distribution(nn.Module):
                     if type(constraint) == constraints.greater_than and constraint.lower_bound == 0:
                         # convert to log-space (for scale parameters)
                         param = torch.log(param)
-                    gate = self.gates[param_name](param_input)
+                    gate = gates[param_name](param_input)
                     param = gate * param + (1. - gate) * param_update
 
                 if param_name == 'loc' and self.manual_loc:
@@ -141,7 +158,7 @@ class Distribution(nn.Module):
                 parameters[param_name] = param
 
             if self.const_scale:
-                log_scale = self.log_scale.repeat(input.shape[0], 1)
+                log_scale = const_log_scale.repeat(input.shape[0], 1)
                 scale = torch.exp(torch.clamp(log_scale, self._log_scale_lim[0], self._log_scale_lim[1]))
                 parameters['scale'] = scale
         elif kwargs is not None:
@@ -149,7 +166,7 @@ class Distribution(nn.Module):
             parameters = kwargs
         else:
             # use the initial parameters
-            parameters = self.initial_params
+            parameters = initial_params
 
         # create a new distribution with the parameters
         if not self.planning:

lib/q_value_estimators/model_based.py
@@ -59,6 +59,16 @@ class ModelBasedEstimator(nn.Module):
         if direct:
             return self.direct_estimate(agent, state, action, target, both, detach_params)
 
+        if target:
+            q_value_models = self.target_q_value_models
+            q_value_variables = self.target_q_value_variables
+        else:
+            q_value_models = self.q_value_models
+            q_value_variables = self.q_value_variables
+        if detach_params:
+            q_value_models = copy.deepcopy(q_value_models)
+            q_value_variables = copy.deepcopy(q_value_variables)
+
         self.planning_mode(agent)
         # set the previous state for residual state prediction
         self.state_variable.cond_likelihood.set_prev_x(state)
@@ -68,24 +78,25 @@ class ModelBasedEstimator(nn.Module):
         for _ in range(self.horizon):
             # estimate the Q-value at current state
             action = action.tanh() if agent.postprocess_action else action
-            q_value_input = [model(state=state, action=action) for model in self.q_value_models]
-            q_values = [variable(inp) for variable, inp in zip(self.q_value_variables, q_value_input)]
+            q_value_input = [model(state=state, action=action) for model in q_value_models]
+            q_values = [variable(inp) for variable, inp in zip(q_value_variables, q_value_input)]
             q_value = torch.min(q_values[0], q_values[1])
             q_values_list.append(q_value)
             # predict state and reward
-            self.generate_state(state, action)
-            self.generate_reward(state, action)
+            self.generate_state(state, action, detach_params)
+            self.generate_reward(state, action, detach_params)
             reward = self.reward_variable.sample()
             rewards_list.append(reward)
             # TODO: give option for deterministic sampling?
             state = self.state_variable.sample()
             # generate the action
-            agent.generate_prior(state)
+            agent.generate_prior(state, detach_params)
             if agent.prior_model is not None:
                 # sample from the learned prior
                 action = agent.prior.sample()
             else:
                 # estimate approximate posterior
+                # TODO: detach params for inference as well
                 agent.inference(state)
                 # calculate KL divergence
                 action = agent.approx_post.sample(agent.n_action_samples)
@@ -94,8 +105,8 @@ class ModelBasedEstimator(nn.Module):
 
         # estimate Q-value at final state
         action = action.tanh() if agent.postprocess_action else action
-        q_value_input = [model(state=state, action=action) for model in self.q_value_models]
-        q_values = [variable(inp) for variable, inp in zip(self.q_value_variables, q_value_input)]
+        q_value_input = [model(state=state, action=action) for model in q_value_models]
+        q_values = [variable(inp) for variable, inp in zip(q_value_variables, q_value_input)]
         q_value = torch.min(q_values[0], q_values[1])
         q_values_list.append(q_value)
 
@@ -137,27 +148,48 @@ class ModelBasedEstimator(nn.Module):
             q_value = torch.min(q_value[0], q_value[1])
         return q_value
 
-    def generate(self, agent):
+    def generate(self, agent, detach_params=False):
         """
         Generate conditional likelihoods for the current state and reward.
-        """
-        prev_act = agent._prev_action.tanh() if agent.postprocess_action else agent._prev_action
-        self.generate_state(agent._prev_state, prev_act)
-        self.generate_reward(agent._prev_state, prev_act)
 
-    def generate_reward(self, state, action):
+        Args:
+            agent (Agent): the agent used to generate state and reward predictions
+            detach_params (bool): whether to use detached (copied) parameters
+        """
+        self.generate_state(agent._prev_state, agent._prev_action, detach_params)
+        self.generate_reward(agent._prev_state, agent._prev_action, detach_params)
+
+    def generate_reward(self, state, action, detach_params=False):
         """
         Generate the conditional likelihood for the reward.
-        """
-        likelihood_input = self.reward_likelihood_model(state=state, action=action)
-        self.reward_variable.generate(likelihood_input, action=action)
 
-    def generate_state(self, state, action):
+        Args:
+            state (torch.Tensor): the input state [batch_size, state_dim]
+            action (torch.Tensor): the input action [batch_size, action_dim]
+            detach_params (bool): whether to use detached (copied) parameters
+        """
+        if detach_params:
+            reward_likelihood_model = copy.deepcopy(self.reward_likelihood_model)
+        else:
+            reward_likelihood_model = self.reward_likelihood_model
+        likelihood_input = reward_likelihood_model(state=state, action=action)
+        self.reward_variable.generate(likelihood_input, action=action, detach_params=detach_params)
+
+    def generate_state(self, state, action, detach_params=False):
         """
         Generate the conditional likelihood for the state.
+
+        Args:
+            state (torch.Tensor): the input state [batch_size, state_dim]
+            action (torch.Tensor): the input action [batch_size, action_dim]
+            detach_params (bool): whether to use detached (copied) parameters
         """
-        likelihood_input = self.state_likelihood_model(state=state, action=action)
-        self.state_variable.generate(likelihood_input)
+        if detach_params:
+            state_likelihood_model = copy.deepcopy(self.state_likelihood_model)
+        else:
+            state_likelihood_model = self.state_likelihood_model
+        likelihood_input = state_likelihood_model(state=state, action=action)
+        self.state_variable.generate(likelihood_input, detach_params=detach_params)
 
     def planning_mode(self, agent):
         """
@@ -181,7 +213,7 @@ class ModelBasedEstimator(nn.Module):
         if self.reward_likelihood_model is not None:
             self.reward_variable.acting_mode()
 
-    def reset(self, batch_size, prev_action, prev_state):
+    def reset(self, batch_size, prev_state):
         """
         Reset the model componenets.
         """