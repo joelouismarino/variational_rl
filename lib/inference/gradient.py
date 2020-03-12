Fixing resampling issue in gradient. Working on direct + gradient.

lib/inference/gradient.py
@@ -22,16 +22,21 @@ class GradientBasedInference(object):
         params = [param for _, param in dist_params.items()]
         act_opt = self.optimizer(params, lr=self.lr)
         act_opt.zero_grad()
-
+        action_list = []
+        import ipdb; ipdb.set_trace()
         for _ in range(self.n_inf_iters):
             actions = agent.approx_post.sample(agent.n_action_samples)
+            action_list.append(actions)
             obj = agent.estimate_objective(state, actions)
             obj = - obj.view(agent.n_action_samples, -1, 1).mean(dim=0)
             self.estimated_objectives.append(obj.detach())
             obj.sum().backward(retain_graph=True)
             act_opt.step()
             act_opt.zero_grad()
+            # clear the sample to force resampling
+            agent.approx_post._sample = None
 
+        import ipdb; ipdb.set_trace()
         clear_gradients(agent.generative_parameters())
 
     def reset(self, *args, **kwargs):