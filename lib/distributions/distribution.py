Fixing gpu issue with quaternion calculation.

lib/distributions/distribution.py
@@ -456,7 +456,7 @@ class Distribution(nn.Module):
         grad_dict = self.get_dist_param_grads()
         # use log-scale as input instead of scale itself
         grad_dict['scale'] = grad_dict['scale'] * param_dict['scale']
-        param_dict['scale'] = param_dict['scale'].log()
+        param_dict['scale'] = (param_dict['scale'] + 1e-6).log()
         # convert to lists
         params = [param.detach() for _, param in param_dict.items()]
         grads = [grad.detach() for _, grad in grad_dict.items()]

misc/euler.py
@@ -196,7 +196,7 @@ def quat_to_rmat(quaternion: torch.Tensor) -> torch.Tensor:
     tyy: torch.Tensor = ty * y
     tyz: torch.Tensor = tz * y
     tzz: torch.Tensor = tz * z
-    one: torch.Tensor = torch.tensor(1.)
+    one: torch.Tensor = torch.tensor(1., device=quaternion.device)
 
     matrix: torch.Tensor = torch.stack([
         one - (tyy + tzz), txy - twz, txz + twy,