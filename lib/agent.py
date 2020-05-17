Fixing possible bug in target inf kl

lib/agent.py
@@ -74,8 +74,10 @@ class Agent(nn.Module):
         # Lagrange multipliers for KL, location KL, and scale KL
         self.log_alphas = nn.ParameterDict({'pi': nn.Parameter(torch.zeros(1)),
                                             'loc': nn.Parameter(torch.zeros(1)),
-                                            'scale': nn.Parameter(torch.zeros(1)),
-                                            'target_inf': nn.Parameter(torch.zeros(1))})
+                                            'scale': nn.Parameter(torch.zeros(1))})
+
+        if misc_args['inf_target_kl']:
+            self.log_alphas['target_inf'] = nn.Parameter(torch.zeros(1))
 
         # miscellaneous
         self.epsilons = misc_args['epsilons']

misc/collector.py
@@ -378,7 +378,7 @@ class Collector:
             self.agent.target_approx_post.reset(self.agent.target_approx_post._batch_size,
                                                 dist_params={'loc': self.agent.target_approx_post.dist.loc.detach(),
                                                              'scale': self.agent.target_approx_post.dist.scale.detach()})
-            kl = kl_divergence(approx_post, self.agent.target_approx_post, n_samples=self.agent.n_action_samples, sample=on_policy_action).sum(dim=1, keepdim=True)
+            kl = kl_divergence(self.agent.approx_post, self.agent.target_approx_post, n_samples=self.agent.n_action_samples, sample=on_policy_action).sum(dim=1, keepdim=True)
             self.metrics['action']['kl_target_inf'].append((kl * (1 - done) * valid).detach())
 
     def _collect_log_probs(self, off_policy_action, log_prob, valid):